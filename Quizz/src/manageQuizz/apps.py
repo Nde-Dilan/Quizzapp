ba944f8916e5dfe60820f555580dcabb3d251ead
Nde-Dilan <125365841+Nde-Dilan@users.noreply.github.com>
2023-04-07 22:27:06 +0100

    Changement complet de dossier

--- Quizz/src/manageQuizz/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class ManagequizzConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'manageQuizz'
--- Quizz/src/manageQuizz/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-from manageQuizz.views import getquizz
-
-urlpatterns = [
-    path('quizz/', getquizz, name='manageQuizz-quizz')
-]
